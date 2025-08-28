70c1920bc229fc26df53e6058166d3e6be869de7
Lama-Moulla <lamamoulla91@gmail.com>
2025-08-28 14:23:50 +0300

    our graduate project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,60 @@
+import {Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './Pages/Home/Home'
+import ContactUS from './Pages/ContactUS/ContactUS'
+import DashBoard from './Pages/DashBoard/DashBoard'
+import Process from './Pages/Process/Process'
+import Work from './Pages/Work/Work'
+import AboutUS from './Pages/AboutUs/AboutUS'
+import LMNavBar from './Components/LMNavBar/LMNavBar'
+import LMFooter from './Components/LMFooter/LMFooter'
+import LMScrollToTop from './Components/LMScrollToTop/LMScrollToTop'
+function App() {
+  return (
+    <>
+      <LMScrollToTop />
+
+      <LMNavBar
+        image="/assets/images/Logo-full.svg"
+        items={[
+          { url: "/", content: "Home" },
+          { url: "/Work", content: "Work" },
+          { url: "/Process", content: "Process" },
+          { url: "/AboutUS", content: "About" },
+          { url: "/DashBoard", content: "DashBoard" },
+        ]}
+        btn="Contact Us"
+        itemsList={[
+          { url: "/", content: "Home" },
+          { url: "/Work", content: "Work" },
+          { url: "/Process", content: "Process" },
+          { url: "/AboutUS", content: "About" },
+        ]}
+      />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/AboutUS' element={<AboutUS />} />
+        <Route path='/ContactUs' element={<ContactUS />} />
+        <Route path='/DashBoard' element={<DashBoard />} />
+        <Route path='/Process' element={<Process />} />
+        <Route path='/Work' element={<Work />} />
+      </Routes>
+
+      <LMFooter
+        image="/assets/images/Logo-full.svg"
+        items={[
+          { url: "/", content: "Home" },
+          { url: "#", content: "Service" },
+          { url: "/Work", content: "Work" },
+          { url: "/Process", content: "Process" },
+          { url: "/AboutUS", content: "About" },
+          { url: "#", content: "Career" },
+          { url: "/ContactUs", content: "Contact" },
+        ]}
+      />
+    </>
+  )
+}
+
+export default App
